57747eddafe3baa3dfc1707b6b9a3360455fe3c0
albonec <agbonecchi@gmail.com>
2022-01-19 16:37:06 -0800

    feature settings menu is stable with all buttons

--- workspace/src/com/albonec/othercode/ui/GuiFeatures.java
+++ workspace/src/com/albonec/othercode/ui/GuiFeatures.java
@@ -1,39 +1,88 @@
 package com.albonec.othercode.ui;
 
+import java.io.IOException;
+import java.util.List;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.ServerSelectionList;
-import net.minecraft.client.multiplayer.ServerList;
-import net.minecraft.client.network.LanServerDetector;
-import net.minecraft.client.resources.I18n;
+import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.util.EnumChatFormatting;
 import org.lwjgl.input.Keyboard;
 
-public class GuiFeatures extends GuiScreen {
-    private GuiScreen parentScreen;
-    private boolean initialized;
+public final class GuiFeatures
+        extends GuiScreen {
 
-    public GuiFeatures(GuiScreen parentScreen) {
-        this.parentScreen = parentScreen;
+    private final GuiScreen previousScreen;
+
+    public GuiFeatures(GuiScreen previousScreen) {
+        this.previousScreen = previousScreen;
     }
 
-    public void initGui()
-    {
+    @Override
+    protected void actionPerformed(GuiButton button) {
+        switch (button.id) {
+            case 1: {
+                this.mc.displayGuiScreen(this.previousScreen);
+                break;
+            }
+            case 0: {
+            }
+        }
+    }
+
+    @Override
+    public void drawScreen(int x2, int y2, float z2) {
+        this.drawDefaultBackground();
+        this.drawCenteredString(this.mc.fontRendererObj, "Feature Settings", width / 2, 20, -1);
+        super.drawScreen(x2, y2, z2);
+    }
+
+    @Override
+    public void initGui() {
+        int var3 = height / 4 + 24;
+        this.buttonList.add(new GuiButton(100, width / 2 - 100, var3 + 72 + 12, "Zoom"));
+        this.buttonList.add(new GuiButton(101, width / 2 - 100, var3 + 72 - 12, "Cheats"));
+        this.buttonList.add(new GuiButton(102, width / 2 - 100, var3 + 72 - 36, "Keystrokes"));
+        this.buttonList.add(new GuiButton(103, width / 2 - 100, var3 + 72 - 60, "Show Coordinates"));
+
+        this.buttonList.add(new GuiButton(1, width / 2 - 100, var3 + 72 + 12 + 24, "Back"));
         Keyboard.enableRepeatEvents(true);
-        this.buttonList.clear();
-
-        if (!this.initialized)
-        {
-            this.initialized = true;
-            this.drawDefaultBackground();
-        }
-        else
-        {
-        }
-        this.createButtons();
     }
 
-    public void createButtons() {
-        this.buttonList.add(new GuiButton(15,this.width / 2 - 154, this.height - 28, 70, 20, I18n.format("Zoom", new Object[0])));
+    @Override
+    protected void keyTyped(char character, int key) {
+        try {
+            super.keyTyped(character, key);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (character == '\t') {
+            } else {
+            }
+        if (character == '\r') {
+            this.actionPerformed((GuiButton)this.buttonList.get(0));
+        }
     }
 
+    @Override
+    protected void mouseClicked(int x2, int y2, int button) {
+        try {
+            super.mouseClicked(x2, y2, button);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onGuiClosed() {
+        Keyboard.enableRepeatEvents(false);
+    }
+
+    @Override
+    public void updateScreen() {
+    }
 }
+
