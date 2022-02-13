6d574270bafcbc59b3d3087dd72ba0fd4233ffef
albonec <agbonecchi@gmail.com>
2022-02-12 21:46:52 -0800

    improved ExceptionHandler.java

--- workspace/src/com/albonec/othercode/multithreading/ExceptionHandler.java
+++ workspace/src/com/albonec/othercode/multithreading/ExceptionHandler.java
@@ -5,7 +5,7 @@ import java.lang.Thread;
 public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
     @Override
     public void uncaughtException(Thread t, Throwable e) {
-        System.out.println("Caught Exception on Thread " + t.getId() + ", " + "Info: " + e.getClass().getName() + " " + e.getMessage());
+        System.out.println("Caught Exception on Thread " + t.getId() + ", " + "Info: " + e.getClass().getName() + " " + e.getMessage() + ", StackTrace: " + e.getStackTrace());
         t.interrupt();
     }
 }
