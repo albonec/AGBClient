34028cd5156c06c38fa497247c7b916db8e21a65
albonec <agbonecchi@gmail.com>
2022-02-20 13:00:02 -0800

    speed calculation updates every tick instead of every second

--- workspace/src/com/albonec/othercode/ui/CustomIngameGui.java
+++ workspace/src/com/albonec/othercode/ui/CustomIngameGui.java
@@ -43,7 +43,7 @@ public class CustomIngameGui extends GuiIngame {
                 new CalcSpeedThread().start();
                 mc.gameSettings.isSpeedThreadOn = true;
             }
-            renderSpeed();
+            renderMotion();
         }
         //renderHP();
         renderArmor();
@@ -156,9 +156,9 @@ public class CustomIngameGui extends GuiIngame {
         }
     }
 
-    private void renderSpeed() {
+    private void renderMotion() {
         ScaledResolution sr = new ScaledResolution(mc);
-        fonts.drawString("Speed: " + this.mc.gameSettings.playerSpeed + " m/s", sr.getScaledWidth() - fonts.getStringWidth("Speed: " + this.mc.gameSettings.playerSpeed + " m/s") - 2, 50, 0x000000);
-        fonts.drawString("Vertical Angle: " + this.mc.gameSettings.vertAngle + " deg", sr.getScaledWidth() - fonts.getStringWidth("Vertical Angle: " + this.mc.gameSettings.vertAngle + " deg") - 2, 60, 0x000000);
+        fonts.drawString("Speed: " + this.mc.gameSettings.playerSpeed + " m/s", sr.getScaledWidth() - fonts.getStringWidth("Speed: " + this.mc.gameSettings.playerSpeed + " m/s") - 2, 50, 0xffffff);
+        fonts.drawString("Vertical Angle: " + this.mc.gameSettings.vertAngle + " deg", sr.getScaledWidth() - fonts.getStringWidth("Vertical Angle: " + this.mc.gameSettings.vertAngle + " deg") - 2, 60, 0xffffff);
     }
 }
