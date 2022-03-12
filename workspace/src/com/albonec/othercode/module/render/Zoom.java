84166b33a49435c1a16387d753b9a29afe4c0a79
albonec <agbonecchi@gmail.com>
2022-03-11 20:59:40 -0800

    zoomthread done

--- workspace/src/com/albonec/othercode/module/render/Zoom.java
+++ workspace/src/com/albonec/othercode/module/render/Zoom.java
@@ -4,10 +4,12 @@ import com.albonec.othercode.event.EventTarget;
 import com.albonec.othercode.event.events.EventUpdate;
 import com.albonec.othercode.module.Category;
 import com.albonec.othercode.module.Module;
+import com.albonec.othercode.multithreading.ZoomThread;
 import com.albonec.othercode.ui.GuiFeaturesScreen;
 import org.lwjgl.input.Keyboard;
 
 public class Zoom extends Module {
+    ZoomThread[] thread = new ZoomThread[1];
 
     private float oldBrightness;
 
@@ -19,19 +21,14 @@ public class Zoom extends Module {
     public void onEnable() {
         super.onEnable();
         oldBrightness = mc.gameSettings.gammaSetting;
-    }
-
-    @EventTarget
-    public void onUpdate(EventUpdate event) {
-        if(mc.gameSettings.doZoom) {
-            mc.gameSettings.gammaSetting = 2.5F;
-        }
+        thread[0] = new ZoomThread();
+        thread[0].start();
     }
 
     @Override
     public void onDisable() {
+        thread[0].interrupt();
         mc.gameSettings.gammaSetting = oldBrightness;
-
         super.onDisable();
     }
 
---
+++ workspace/src/com/albonec/othercode/multithreading/ZoomThread.java
@@ -0,0 +1,37 @@
+package com.albonec.othercode.multithreading;
+
+import net.minecraft.client.Minecraft;
+import org.lwjgl.input.Keyboard;
+
+public class ZoomThread extends Thread {
+    private Minecraft mc = Minecraft.getMinecraft();
+
+    @Override
+    public void run() {
+        Thread.currentThread().setUncaughtExceptionHandler(new ExceptionHandler());
+        while (!this.isInterrupted()) {
+            try {
+                if (Keyboard.isKeyDown(Keyboard.KEY_UP) && mc.gameSettings.gammaSetting >= 0.0F) {
+                    mc.gameSettings.gammaSetting -= Math.sqrt(mc.gameSettings.gammaSetting + PressedZoomKey.UP.increase);
+                    Thread.sleep(50);
+                }
+                if (Keyboard.isKeyDown(Keyboard.KEY_DOWN) && mc.gameSettings.gammaSetting <= 70.0F) {
+                    mc.gameSettings.gammaSetting += Math.sqrt(mc.gameSettings.gammaSetting + PressedZoomKey.UP.increase);
+                    Thread.sleep(50);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    enum PressedZoomKey {
+        UP(-0.25F),
+        DOWN(0.25F);
+
+        public final float increase;
+
+        PressedZoomKey(float increase) {this.increase = increase;}
+    }
+
+}
