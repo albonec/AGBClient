e60ca799ba7e6319de735876f15a44f9c06fcbf1
albonec <70042211+albonec@users.noreply.github.com>
2022-11-25 20:37:55 -0800

    minor changes/revisions

--- workspace/src/com/albonec/othercode/multithreading/ParkourThread.java
+++ workspace/src/com/albonec/othercode/multithreading/ParkourThread.java
@@ -8,8 +8,20 @@ public class ParkourThread extends Thread{
 
     private Minecraft mc = Minecraft.getMinecraft();
 
+    /*
+    n^n+1/n+1
+     */
+
+    private String getArcFunction(int[] blockCoordinates) {
+        double secantSlope2D = (mc.thePlayer.posY - blockCoordinates[1]) / (mc.thePlayer.posX - blockCoordinates[0]);
+        double secantYIntercept = mc.thePlayer.posY - blockCoordinates[1];
+
+        String function = Math.abs(secantSlope2D/2) + "x^2" + (secantYIntercept < 0 ? " - " : " + ") + Math.abs(secantYIntercept) + "x" + " + " + secantYIntercept;
+        return function;
+    }
+
     private double calcDistance(double x, double y, double z) {
-        return Math.hypot(Math.abs(z), Math.hypot(Math.abs(x), Math.abs(y)));
+        return Math.hypot(Math.abs(y), Math.hypot(Math.abs(x), Math.abs(z)));
     }
 
     @Override
